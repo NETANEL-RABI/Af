c91a1858bf686542bcf3c93e15dc47e9cde47f79
NETANEL-RABI <n0527648116@gmail.com>
2026-06-28 00:03:18 +0300

    Remove default values from VideoInfo properties

--- app/src/main/java/com/videodownloader/model/VideoInfo.kt
+++ app/src/main/java/com/videodownloader/model/VideoInfo.kt
@@ -4,7 +4,7 @@ data class VideoInfo(
     val title: String,
     val thumbnailUrl: String,
     val downloadUrl: String,
-    val fileSize: String = "לא ידוע",
-    val duration: String = "",
-    val source: String = ""
+    val fileSize: String,
+    val duration: String,
+    val source: String
 )
